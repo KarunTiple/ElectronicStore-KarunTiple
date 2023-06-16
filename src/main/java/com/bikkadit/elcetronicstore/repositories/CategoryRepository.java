93aeb4d298e9b7ffab010688047d4cd8c92a3b52
karuntiple <tiplekarun@gmail.com>
2023-06-16 17:00:03 +0530

    updates in Category Repository

--- src/main/java/com/bikkadit/elcetronicstore/repositories/CategoryRepository.java
+++ src/main/java/com/bikkadit/elcetronicstore/repositories/CategoryRepository.java
@@ -3,5 +3,5 @@ package com.bikkadit.elcetronicstore.repositories;
 import com.bikkadit.elcetronicstore.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
+public interface CategoryRepository extends JpaRepository<Category, String> {
 }
