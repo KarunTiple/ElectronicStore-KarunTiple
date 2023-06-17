7fa3db01a4a7b5fc0945982b5ea4aa0dd65fc5f0
karuntiple <tiplekarun@gmail.com>
2023-06-17 12:57:28 +0530

    ProductRepository is created.

---
+++ src/main/java/com/bikkadit/elcetronicstore/repositories/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.bikkadit.elcetronicstore.repositories;
+
+import com.bikkadit.elcetronicstore.entities.Products;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Products, String> {
+
+    //search
+    Page<Products> findByTitleContaining(String keyword,Pageable pageable);
+
+    Page<Products> findByLiveTrue(Pageable pageable);
+
+}
